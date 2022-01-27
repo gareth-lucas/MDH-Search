0f693defd5aa90eae21840cc34a0a478093dfc12
Gareth Lucas <gareth.lucas@gmail.com>
2022-01-27 15:41:54 +0100

    http backend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 require('dotenv').config();
+const https = require('https');
 const express = require('express')
 const cors = require('cors');
 const apiRoutes = require('./routes/api.routes');
@@ -7,6 +8,15 @@ const securityRoutes = require('./routes/security.routes');
 const adminRoutes = require('./routes/admin.routes');
 const profileRoutes = require('./routes/profile.routes');
 const { path } = require('express/lib/application');
+const fs = require('fs');
+
+var privateKey = fs.readFileSync(process.env.SSL_CERT_KEY);
+var certificate = fs.readFileSync(process.env.SSL_CERTIFICATE);
+
+const options = {
+    key: privateKey,
+    cert: certificate
+};
 
 const app = express()
 app.use(express.json());
@@ -21,6 +31,6 @@ app.use('/profile', profileRoutes);
 // #TODO: General error handler + catch all 404
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+https.createServer(options, app).listen(port, () => {
     console.log(`MDH Search Tool listening at http://localhost:${port}`)
 })
